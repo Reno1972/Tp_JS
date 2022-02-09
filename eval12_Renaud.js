ccc0efa7e3f62033f8fc90ef2a30f7d9decae584
Renaud <renaud.colomar@gmail.com>
2022-02-09 16:47:38 +0100

    Ajout TP12

--- eval12_Renaud.js
+++ eval12_Renaud.js
@@ -1,40 +1,43 @@
 do{
-    var question=parseInt(prompt("Quel calcul souhaitez-vous faire ?\n - 1 - Addition\n - 2 - Soustraction\n - 3 - Multiplication\n - 4 - Division"));
-} while(question<1 || question>4);
+    do{
+        var question=parseInt(prompt("Quel calcul souhaitez-vous faire ?\n - 1 - Addition\n - 2 - Soustraction\n - 3 - Multiplication\n - 4 - Division"));
+    } while(question<1 || question>4);
 
-let a=prompt("Entrez la première valeur :");
-a=+a;
+    let a=prompt("Entrez la première valeur :");
+    a=+a;
 
-let b=prompt("Entrez la deuxième valeur :");
-b=+b
+    let b=prompt("Entrez la deuxième valeur :");
+    b=+b
 
-function addition(){
-    return a+b;
-}
-function soustraction(){
-    return a-b;
-}
-function multibication(){
-    return a*b;
-}
-function division(){
-    return a/b;
-}
+    function addition(){
+        return a+b;
+    }
+    function soustraction(){
+        return a-b;
+    }
+    function multibication(){
+        return a*b;
+    }
+    function division(){
+        return a/b;
+    }
 
-switch(question){
-    case 1:
-        alert("Le resultat de l'addition de "+a+"+"+b+" est : "+addition(a,b));
-        break;
-    case 2:
-        alert("Le résultat de la soustraction"+a+"-"+b+" est : "+soustraction(a,b));
-        break;
-    case 3:
-        alert("Le résultat de la multibication"+a+"x"+b+" est : "+multibication(a,b));
-        break;
-    case 4:
-        alert("Le résultat de la division "+a+"/"+b+" est : "+division(a,b));
-        break;
-default:
-    alert("Calcul impossible.");
-}
+    switch(question){
+        case 1:
+            alert("Le resultat de l'addition de "+a+"+"+b+" est : "+addition(a,b));
+            break;
+        case 2:
+            alert("Le résultat de la soustraction"+a+"-"+b+" est : "+soustraction(a,b));
+            break;
+        case 3:
+            alert("Le résultat de la multibication"+a+"x"+b+" est : "+multibication(a,b));
+            break;
+        case 4:
+            alert("Le résultat de la division "+a+"/"+b+" est : "+division(a,b));
+            break;
+    default:
+        alert("Calcul impossible.");
+    }
+result=confirm("Voulez-vous effectuer un autre calcul ?");
+}while(result==true);
 alert("Calcul terminé, au revoir !");
